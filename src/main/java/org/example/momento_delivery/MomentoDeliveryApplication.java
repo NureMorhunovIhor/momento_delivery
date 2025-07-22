67589d420d309d3f639656b23af0c7fcdc54700d
NureMorhunovIhor <lapaspalma@gmail.com>
2025-07-22 18:09:25 +0300

    Initial commit

---
+++ src/main/java/org/example/momento_delivery/MomentoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.example.momento_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentoDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentoDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/momento_delivery/MomentoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.momento_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentoDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
